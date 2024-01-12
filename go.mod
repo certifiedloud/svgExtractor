068213a3dce56ece5bde0e13be23290d3442f0f1
Cody Heimberger <cody.heimberger@broadcom.com>
2024-01-12 12:10:30 -0600

    initial commit

---
+++ go.mod
@@ -0,0 +1,23 @@
+module svgextractor
+
+go 1.18
+
+require (
+	github.com/chromedp/chromedp v0.9.3
+	github.com/urfave/cli/v2 v2.27.1
+	golang.org/x/net v0.20.0
+)
+
+require (
+	github.com/chromedp/cdproto v0.0.0-20240102194822-c006b26f21c7 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.3.2 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	// "time"
+
+	"github.com/chromedp/chromedp"
+	// "github.com/gostaticanalysis/nilerr"
+	"github.com/urfave/cli/v2"
+	"golang.org/x/net/html"
+)
+
+func main() {
+	var url, class string
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "class",
+				Usage: "Css class of desired SVGs",
+			},
+			&cli.StringFlag{
+				Name:  "id",
+				Usage: "Css ID of desired SVGs",
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			// var url string
+			if cCtx.NArg() < 1 {
+				return fmt.Errorf("You must provide a URL")
+			} else {
+				url = cCtx.Args().Get(0)
+			}
+			if cCtx.String("class") != "" {
+				class = cCtx.String("class")
+			}
+			// if cCtx.String("id") != "" {
+			// 	id = cCtx.String("id")
+			// }
+			svgSources, err := fetchSVGsByClass(url, class)
+			if err != nil {
+				log.Fatal(err)
+			}
+			for i, svgSource := range svgSources {
+				filename := fmt.Sprintf("svg_%d.svg", i+1)
+				if err := saveSVGToFile(filename, svgSource); err != nil {
+					log.Printf("Error saving SVG %d: %v", i+1, err)
+					return err
+				}
+			}
+
+			return nil
+
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func fetchSVGsByClass(url, className string) ([]string, error) {
+	// Create a new context
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	// Navigate to the specified URL
+	if err := chromedp.Run(ctx, chromedp.Navigate(url)); err != nil {
+		return nil, err
+	}
+
+	// Wait for JavaScript to execute (adjust the duration as needed)
+	// time.Sleep(2 * time.Second)
+
+	// Capture the HTML content after JavaScript execution
+	var htmlContent string
+	if err := chromedp.Run(ctx, chromedp.OuterHTML("html", &htmlContent)); err != nil {
+		return nil, err
+	}
+
+	// Parse the HTML document
+	doc, err := html.Parse(strings.NewReader(htmlContent))
+	if err != nil {
+		return nil, err
+	}
+
+	// Find all SVG elements with the specified class
+	var svgSources []string
+	var findSVG func(*html.Node)
+	findSVG = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "svg" {
+			// Check if the SVG has the specified class
+			for _, attr := range n.Attr {
+				if attr.Key == "class" && strings.Contains(attr.Val, className) {
+					// Serialize the HTML of the SVG element
+					var sb strings.Builder
+					html.Render(&sb, n)
+					svgSources = append(svgSources, sb.String())
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			findSVG(c)
+		}
+	}
+
+	findSVG(doc)
+
+	return svgSources, nil
+}
+
+func saveSVGToFile(filename, svgContent string) error {
+	return ioutil.WriteFile(filename, []byte(svgContent), 0644)
+}
